b7d9762e933e98b9decc7f430096752208be7d54
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-21 08:58:53 +0200

    Add project files.

---
+++ Negozio_13/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NegozioEs12;
+using ScontoNegozioN13;
+
+namespace Negozio_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci il prezzo totale: ");
+            double totale = double.Parse(Console.ReadLine());
+            double n = ScontoNegozio.Negozio(totale);
+            double n12 = ScontoNegozio12.Negozio12(totale);
+            if (n < n12)
+            {
+                Console.WriteLine("conviene il primo negozio");
+            }
+            else if (n > n12)
+            {
+                Console.WriteLine("conviene il primo negozio");
+            }
+            else
+            {
+                Console.WriteLine("Sono ugualmente convenienti");
+            }
+            Console.ReadLine();
+        }
+    }
+}
